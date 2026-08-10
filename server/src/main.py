4e37f10e838c983c7f4d81805324f9543e2f32b5
rgraue <rgraue@outlook.com>
2026-08-09 22:42:39 -0700

    docker yay

--- server/src/main.py
+++ server/src/main.py
@@ -10,7 +10,7 @@ def main():
     # add routers
     api.include_router(example_router)
 
-    uvicorn.run(api)
+    uvicorn.run(api, host="0.0.0.0", port=11818)
 
 if __name__ == '__main__':
     main()
